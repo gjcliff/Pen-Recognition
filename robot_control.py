f93b386d8f4412ba8061992d5cbd292e3122324b
Graham Clifford <gjcliff@gmail.com>
2023-09-15 21:25:05 -0500

    hello

--- robot_control.py
+++ robot_control.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python
 
-# from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
-# import time
-# # The robot object is what you use to control the robot
-
-# robot_joints = ["base_link", "ee_arm_link", "ee_gripper_link", "fingers_link", "forearms_link", "gripper_bar_link", "gripper_link", "gripper_prop_link", "left_finger_link", "right_finger_link", "shoulder_link", "upper_arm_link"]
-
-# robot = InterbotixManipulatorXS("px100", "arm", "gripper")
-# mode = 'h'
-# # Let the user select the position
-# # while mode != 'q':
-# #     mode=input("[h]ome, [s]leep, [g] joint commands, [q]uit ")
-# #     if mode == "h":
-# #         robot.arm.go_to_home_pose()
-# #     elif mode == "s":
-# #         robot.arm.go_to_sleep_pose()
-# #     else:
-# #         joint_command = robot.arm.get_ee_pose()
-# #         print(f"current joint angles: {joint_command}")
-
-# # robot.arm.get_joint_commands()
-# print(robot.arm.get_ee_pose())
-
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
 import numpy as np
+import math
 
 # This script makes the end-effector perform pick, pour, and place tasks
 # Note that this script may not work for every arm as it was designed for the wx250
@@ -33,7 +12,7 @@ import numpy as np
 # Then change to this directory and type 'python bartender.py  # python3 bartender.py if using ROS Noetic'
 
 bot = InterbotixManipulatorXS("px100", "arm", "gripper")
-ee_origin = [160, 412, -90] # from camera perspective: (y,x,z)
+ee_origin = [150, 436, -90] # from camera perspective: (y,x,z)
 
 def camera_coor_to_robot_coor(camera_point):
     robot_point = [-float(camera_point[1]),float(camera_point[0]),float(camera_point[2])]
@@ -54,30 +33,33 @@ def main():
     print(f"x move: {ee_origin[0] - robot_point[0]}, y move: {ee_origin[1] - robot_point[1]}, z move: {ee_origin[2] - robot_point[2]}")
     # bot.arm.go_to_home_pose()robot_point
 
+    y = ee_origin[1] - robot_point[1]
+    x = ee_origin[0] - robot_point[0]
+    print(f"y: {y}, x: {x}")
+    angle = np.arctan2(y,x)
+    print(f"angle: {angle}")
+
     # Differences
-    xDiff = ee_origin[0] - robot_point[0]
-    yDiff = ee_origin[1] - robot_point[1]
+    xDiff = math.sqrt((robot_point[0]-ee_origin[0])**2 + (robot_point[1]-ee_origin[1])**2)
+    print(xDiff)
     zDiff = ee_origin[2] - robot_point[2]
 
     # direction factors
-    xDir = xDiff/abs(xDiff)
-    yDir = yDiff/abs(yDiff)
-    zDir = zDiff/abs(zDiff)
+    xDir = xDiff / abs(xDiff)
+    zDir = zDiff / abs(zDiff)
 
     xD = (xDiff * xDir)/1000
-    # if ee_origin[0] > robot_point[0]: xD *= -1
-
-    yD = (yDiff * yDir)/1000
-    # if ee_origin[1] > robot_point[1]: yD *= -1
 
     zD = (zDiff * zDir)/1000
 
-    print(f"xD: {xD}, yD: {yD}, zd: {zD}")
+    print(f"xD: {xD} zd: {zD}")
 
     # bot.gripper.grasp(2.0)
     # bot.gripper.set_pressure(1.0)
 
     try:
+        bot.arm.go_to_sleep_pose()
+        bot.arm.set_single_joint_position("waist",-angle)
         bot.arm.set_ee_cartesian_trajectory(x=xD,z=zD)
         bot.gripper.grasp(1)
         bot.arm.go_to_sleep_pose()
