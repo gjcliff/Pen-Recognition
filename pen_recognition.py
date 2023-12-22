16bb3b7e115d7a9fd11638a1241d3ab37558dcd7
Graham Clifford <gjcliff@gmail.com>
2023-12-21 20:20:53 -0500

    Added a README

--- pen_recognition.py
+++ pen_recognition.py
@@ -7,8 +7,10 @@ import numpy as np
 # Import OpenCV for easy image rendering
 import cv2
 
+
 def nothing(x):
     pass
+
 
 def main():
     # Create a pipeline
@@ -68,17 +70,17 @@ def main():
     align_to = rs.stream.color
     align = rs.align(align_to)
 
-    #opencv
+    # opencv
     cv2.namedWindow('image')
 
     # create trackbars for color change
-    cv2.createTrackbar('H1','image',207,360, nothing)
-    cv2.createTrackbar('S1','image',90,255, nothing)
-    cv2.createTrackbar('V1','image',0,255, nothing)
+    cv2.createTrackbar('H1', 'image', 207, 360, nothing)
+    cv2.createTrackbar('S1', 'image', 90, 255, nothing)
+    cv2.createTrackbar('V1', 'image', 0, 255, nothing)
 
-    cv2.createTrackbar('H2','image',305,360, nothing)
-    cv2.createTrackbar('S2','image',255,255, nothing)
-    cv2.createTrackbar('V2','image',255,255, nothing)
+    cv2.createTrackbar('H2', 'image', 305, 360, nothing)
+    cv2.createTrackbar('S2', 'image', 255, 255, nothing)
+    cv2.createTrackbar('V2', 'image', 255, 255, nothing)
 
     # Streaming loop
 
@@ -87,18 +89,19 @@ def main():
     try:
         while True:
             # get current positions of four trackbars
-            h1 = cv2.getTrackbarPos('H1','image')
-            s1 = cv2.getTrackbarPos('S1','image')
-            v1 = cv2.getTrackbarPos('V1','image')
+            h1 = cv2.getTrackbarPos('H1', 'image')
+            s1 = cv2.getTrackbarPos('S1', 'image')
+            v1 = cv2.getTrackbarPos('V1', 'image')
 
-            h2 = cv2.getTrackbarPos('H2','image')
-            s2 = cv2.getTrackbarPos('S2','image')
-            v2 = cv2.getTrackbarPos('V2','image')
+            h2 = cv2.getTrackbarPos('H2', 'image')
+            s2 = cv2.getTrackbarPos('S2', 'image')
+            v2 = cv2.getTrackbarPos('V2', 'image')
 
             # Get frameset of color and depth
             frames = pipeline.wait_for_frames()
             aligned_frames = align.process(frames)
-            aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+            # aligned_depth_frame is a 640x480 depth image
+            aligned_depth_frame = aligned_frames.get_depth_frame()
             depth_frame = decimation_filter.process(aligned_depth_frame)
             depth_frame = spatial_filter.process(depth_frame)
             depth_frame = temporal_filter.process(depth_frame)
@@ -119,8 +122,8 @@ def main():
             # print(s)
             # print(type(s))
 
-            lower_bound = np.array([h1/2,s1,v1])
-            upper_bound = np.array([h2/2,s2,v2])
+            lower_bound = np.array([h1/2, s1, v1])
+            upper_bound = np.array([h2/2, s2, v2])
 
             # threshold the HSV image to get purple colors within our range
             mask = cv2.inRange(hsv, lower_bound, upper_bound)
@@ -128,57 +131,56 @@ def main():
             # bitwise-AND mask and original image
             # masked_image = cv2.bitwise_and(color_image, color_image, mask= mask)
 
-            eroded_img = cv2.erode(mask, np.ones((4,4)))
-            dilated_img = cv2.erode(eroded_img, np.ones((4,4)))
+            eroded_img = cv2.erode(mask, np.ones((4, 4)))
+            dilated_img = cv2.erode(eroded_img, np.ones((4, 4)))
 
-            closed_img = cv2.morphologyEx(dilated_img, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)), iterations=5)
+            closed_img = cv2.morphologyEx(dilated_img, cv2.MORPH_CLOSE, cv2.getStructuringElement(
+                cv2.MORPH_RECT, (5, 5)), iterations=5)
 
             # drawing contours
-            contours, hierarchy = cv2.findContours(closed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(color_image, contours, -1, (0,255,0), 3)
+            contours, hierarchy = cv2.findContours(
+                closed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(color_image, contours, -1, (0, 255, 0), 3)
 
             depth_mask = cv2.bitwise_and(depth_image, depth_image, mask=mask)
 
             try:
-                max_contour = max(contours, key = cv2.contourArea)
+                max_contour = max(contours, key=cv2.contourArea)
                 M = cv2.moments(max_contour)
-                
+
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
 
                 depth_area = depth_mask[cy-5:cy+5, cx-5:cx+5]
                 depth = depth_area.mean()
 
-                point = rs.rs2_deproject_pixel_to_point(intr, [cx,cy], depth)
-                point = [point[2],point[0],-point[1]]
+                point = rs.rs2_deproject_pixel_to_point(intr, [cx, cy], depth)
+                point = [point[2], point[0], -point[1]]
 
                 points.append(point)
-                # print(f"point: {point}")
 
                 if len(points) > 50:
                     points_array = np.array(points)
-                    avg_x = np.mean(points_array[:,0])
-                    avg_y = np.mean(points_array[:,1])
-                    avg_z = np.mean(points_array[:,2])
+                    avg_x = np.mean(points_array[:, 0])
+                    avg_y = np.mean(points_array[:, 1])
+                    avg_z = np.mean(points_array[:, 2])
 
-                    std_x = np.std(points_array[:,0])
-                    std_y = np.std(points_array[:,1])
-                    std_z = np.std(points_array[:,2])
-
-                    # print(f"avg_x: {avg_x}, avg_y: {avg_y}, avg_z: {avg_z}")
-                    # print(f"std_x: {std_x}, std_y: {std_y}, std_z: {std_z}")
+                    std_x = np.std(points_array[:, 0])
+                    std_y = np.std(points_array[:, 1])
+                    std_z = np.std(points_array[:, 2])
 
                     points.clear()
 
                     thresh = 30
-                    
-                    with open("fifo_empty.txt","r") as f2:
+
+                    with open("fifo_empty.txt", "r") as f2:
                         if std_x < thresh and std_y < thresh and std_z < thresh and f2.read() == "0":
                             point = [avg_x, avg_y, avg_z]
                             print("writing to fifo")
                             print(f"point: {point[0]}, {point[1]}, {point[2]}")
                             f1.write("")
-                            f1.write(str(point[0]) + "\n" + str(point[1]) + "\n" + str(point[2]) + "\n")
+                            f1.write(
+                                str(point[0]) + "\n" + str(point[1]) + "\n" + str(point[2]) + "\n")
                             f1.flush()
                             f2.close()
 
@@ -187,9 +189,10 @@ def main():
 
                 text = f"(x: {round(point[0])}, y: {round(point[1])}, z: {round(point[2])})"
 
-                color_image = cv2.putText(color_image, text, (cx-100,cy+40),cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,255,255), 1)
+                color_image = cv2.putText(
+                    color_image, text, (cx-100, cy+40), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
 
-                cv2.circle(color_image,(cx,cy),10,(255,255,255),-1)
+                cv2.circle(color_image, (cx, cy), 10, (255, 255, 255), -1)
             except:
                 pass
 
@@ -202,17 +205,16 @@ def main():
             # cv2.imshow('mask', mask)
             # cv2.imshow('depth', depth_colormap)
             cv2.imshow('image', color_image)
-            
+
             key = cv2.waitKey(1) & 0xFF
             if key & 0xFF == ord('q') or key == 27:
                 cv2.destroyAllWindows()
                 break
 
-            
-
     finally:
         pipeline.stop()
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
